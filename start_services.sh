8445c8693fb5a7c5d3636e703796018d69a3993e
sabiralipsl <157841468+sabiralipsl@users.noreply.github.com>
2024-02-02 22:08:35 +0500

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -14,9 +14,9 @@ sudo -u xtreamcodes /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtream
 sudo -u xtreamcodes /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/tools/signal_receiver.php >/dev/null 2>/dev/null &
 sudo -u xtreamcodes /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/tools/pipe_reader.php >/dev/null 2>/dev/null &
 chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb 2>/dev/null
-wget -qO /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb https://github.com/dOC4eVER/ubuntu20.04/releases/download/start/GeoLite2.mmdb 2>/dev/null
+wget -qO /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb https://github.com/sabiralipsl/jaan/releases/download/sabir/GeoLite2.mmdb 2>/dev/null
 chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb 2>/dev/null
-geoliteversion=$(wget -qO- https://github.com/dOC4eVER/ubuntu20.04/releases/download/start/Geolite2_status.json | jq -r ".version")
+geoliteversion=$(wget -qO- https://github.com/sabiralipsl/jaan/releases/download/sabir/Geolite2_status.json | jq -r ".version")
 PASSMYSQL=$(python2 /home/xtreamcodes/iptv_xtream_codes/pytools/config.py DECRYPT | grep Password | sed "s|Password:            ||g")
 mysql -u user_iptvpro -p$PASSMYSQL -P 7999 xtream_iptvpro -e "UPDATE admin_settings SET value = '$geoliteversion' WHERE admin_settings.type = 'geolite2_version'; " 2>/dev/null
 chown -R xtreamcodes:xtreamcodes /sys/class/net 2>/dev/null
