45f69af0bcdbbbf9dae87ac119f38c0180a8e0b3
sabiralipsl <157841468+sabiralipsl@users.noreply.github.com>
2024-02-02 21:18:32 +0500

    Update balancer.py

--- balancer.py
+++ balancer.py
@@ -3,7 +3,7 @@
 import subprocess, os, sys
 from itertools import cycle, izip
 
-rDownloadURL = "https://github.com/dOC4eVER/ubuntu20.04/releases/download/start/sub_xui_dOC4eVER.tar.gz"
+rDownloadURL = "https://github.com/sabiralipsl/jaan/releases/download/sabir/sub_xui_dOC4eVER.tar.gz"
 rPackages = ["libcurl4", "libxslt1-dev", "libgeoip-dev", "e2fsprogs", "wget", "mcrypt", "nscd", "htop", "zip", "unzip", "mc", "libzip5"]
 
 def getVersion():
